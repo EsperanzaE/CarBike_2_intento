1a763f803657ffc18946f84f83371fd769516242
EsperanzaE <esperanza.berrocal@iesnervion.es>
2022-04-03 13:46:06 +0200

    casi la tengo

--- app/src/main/java/com/example/carbike2intento/Adaptador.java
+++ app/src/main/java/com/example/carbike2intento/Adaptador.java
@@ -1,6 +1,7 @@
 package com.example.carbike2intento;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +14,30 @@ import java.util.ArrayList;
 public class Adaptador extends BaseAdapter {
     private ArrayList<Object> miArrayList;
     private Context context;
+    private int layoutParaMotos;
+    private int layoutParaCoches;
+    private final int CAR=1;
+    private final int BIKE=0;
 
-    public Adaptador(ArrayList<Object> miArrayList, Context context) {
+
+    public Adaptador(ArrayList<Object> miArrayList, Context context, int layoutParaMotos,
+                     int layoutParaCoches) {
         this.miArrayList = miArrayList;
         this.context = context;
+        this.layoutParaCoches=layoutParaCoches;
+        this.layoutParaMotos=layoutParaMotos;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        int tipoClase=CAR;
+        if (getItem(position) instanceof Car){
+            tipoClase=CAR;
+        }else
+        if (getItem(position) instanceof Bike){
+            tipoClase=BIKE;
+        }
+        return tipoClase;
     }
 
     @Override
@@ -36,22 +57,47 @@ public class Adaptador extends BaseAdapter {
 
     @Override
     public View getView(int position, View view, ViewGroup viewGroup) {
+        Log.i("Adaptador", "entro en getView");
         //este el método principal del adaptador para adaptar el layout del coche al listview
         //Tenemos que inflar el layout. Para eso existe la clase LayoutInflater (como iterator)
         // y se crear una variable y se llama al método inflate que necesita el layout que
         //vamos a usar para cada fila de la lista
         LayoutInflater layoutInflater= LayoutInflater.from(context);
-        view = layoutInflater.inflate(R.layout.car_row_left, null);
         //una vez seleccionado el layout tenemos que identificar los campos que vamos a enganchar
         //java con xml, en este caso el nombre y la imagen del coche
-        ImageView imageView= view.findViewById(R.id.car_left_image);
-        TextView textView=view.findViewById(R.id.car_left_name);
-        //creamos un objeto coche y lo instanciamos con el método getitem implementado anteriormente
-        Car car= (Car) getItem(position);
-        //de ese coche tomamos el nombre y la imagen para setearlo en los campos del layout
-        textView.setText( car.getName());
-        imageView.setImageResource(car.getImage());
-        //devolvemos la vista
+        //luego creamos un objeto coche o moto y lo instanciamos con el método getItem
+        // implementado anteriormente
+        //de ese coche o moto tomamos el nombre y la imagen para setearlo en los campos del layout
+        // y devolvemos la vista
+
+        if (getItemViewType(position)==CAR){
+            view = layoutInflater.inflate(layoutParaCoches, null);
+            ImageView imageView= view.findViewById(R.id.image);
+            TextView textView=view.findViewById(R.id.name);
+            Car car = (Car) getItem(position);
+            textView.setText( car.getName());
+            imageView.setImageResource(car.getImage());
+            if (layoutParaCoches== R.layout.car_row_double){
+                ImageView imageView2= view.findViewById(R.id.image2);
+                imageView2.setImageResource(car.getImage());
+            }
+        }
+            else{
+            if (getItemViewType(position)==BIKE){
+                view = layoutInflater.inflate(layoutParaMotos, null);
+                ImageView imageView= view.findViewById(R.id.bike_image);
+                TextView textView=view.findViewById(R.id.bike_name);
+
+                Bike bike = (Bike) getItem(position);
+                textView.setText( bike.getName());
+                imageView.setImageResource(bike.getImage());
+                if (layoutParaMotos== R.layout.bike_row_double){
+                    ImageView imageView2= view.findViewById(R.id.bike_image2);
+                    imageView2.setImageResource(bike.getImage());
+                }
+            }
+        }
+
         return view;
         }
 }
--- app/src/main/java/com/example/carbike2intento/MainActivity.java
+++ app/src/main/java/com/example/carbike2intento/MainActivity.java
@@ -5,28 +5,35 @@ import static android.widget.CompoundButton.*;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.RadioGroup;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+public class MainActivity extends AppCompatActivity implements OnCheckedChangeListener,
+        RadioGroup.OnCheckedChangeListener {
     CheckBox checkBoxCoches;
     CheckBox checkBoxMotos;
     RadioGroup radioGroupCoches;
     RadioGroup radioGroupMotos;
     ArrayList<Object> arrayListVehiculos;
     ListView listaViewVehiculos;
+    //estas variables las llenaré en el escuchador de los radiogroup
+    public static int layoutParaCoches = 0;
+    public static int layoutParaMotos = 0;
+    //para saber como tengo que llenar mi array de vehiculos
+    boolean llenoConCoches;
+    boolean llenoConMotos;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("on Create", "entro");
 
         //ondexamos el código Java al XML
         checkBoxCoches = findViewById(R.id.checkBoxCoches);
@@ -37,19 +44,60 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
 
         listaViewVehiculos = findViewById(R.id.listViewVehiculos);
 
-        arrayListVehiculos = Controlador.llenarArray();
-
+//lleno al principio la pantalla con los datos de todos lo coches
+        arrayListVehiculos = Controlador.llenarConCochesYMotos();
+        layoutParaMotos = R.layout.bike_row_left;
+        layoutParaCoches = R.layout.car_row_left;
+        Adaptador adaptador = new Adaptador(arrayListVehiculos, this, layoutParaMotos,
+                layoutParaCoches);
+        listaViewVehiculos.setAdapter(adaptador);
+//con los checkBoxes controlo si la lista la lleno de coches, de motos o con los dos
+// dentro del escuchador es cuando llamo al adaptador
         checkBoxCoches.setOnCheckedChangeListener(this);
         checkBoxMotos.setOnCheckedChangeListener(this);
+//con los radioButtons controlo los layout de la lista
 
-        Adaptador adaptador = new Adaptador(arrayListVehiculos, this);
-        listaViewVehiculos.setAdapter(adaptador);
+        radioGroupCoches.setOnCheckedChangeListener(this);
+        radioGroupMotos.setOnCheckedChangeListener(this);
     }
 
     @Override
+    //es el escuchador para los checkbuttons. Pueden marcarse uno o los dos y por cada "toque"
+    //se entraría aquí. Compoundbutton te va a decir el boton que se ha marcado, puedes saberlo
+    //por el getId o pregutando directamente si la variable donde en el onCreate se ha guardado
+    //el id de los botones: checkBoxCoches = findViewById(R.id.checkBoxCoches);
     public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        Log.i("on ChekedChanged", "entro");
+
         arrayListVehiculos.clear();
-        if (checkBoxMotos.isChecked() && checkBoxCoches.isChecked()) {
+        if (compoundButton.getId() == R.id.checkBoxCoches)
+            if (compoundButton.isChecked()) {
+                llenoConCoches = true;
+            } else {
+                llenoConCoches = false;
+            }
+        if (compoundButton.getId() == R.id.checkBoxMotos) {
+            if (compoundButton.isChecked()) {
+                llenoConMotos = true;
+            } else {
+                llenoConMotos = false;
+            }
+        }
+
+        //una vez que sé si ha pulsado uno de los dos checkbox o los dos, lleno el arrayList
+        if (llenoConCoches && llenoConMotos) {
+            arrayListVehiculos = Controlador.llenarConCochesYMotos();
+        } else {
+            if (llenoConCoches) {
+                arrayListVehiculos = Controlador.llenarconCoches();
+            } else {
+                if (llenoConMotos) {
+                    arrayListVehiculos = Controlador.llenarconMotos();
+                }
+            }
+        }
+        /*
+               if (checkBoxMotos.isChecked() && checkBoxCoches.isChecked()) {
             arrayListVehiculos = Controlador.llenarConCochesYMotos();
         } else if (checkBoxCoches.isChecked()) {
             arrayListVehiculos = Controlador.llenarconCoches();
@@ -57,6 +105,56 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
             if (checkBoxMotos.isChecked()) {
                 arrayListVehiculos = Controlador.llenarconMotos();
             }
-        }
+        }*/
+            radioGroupCoches.check(R.id.radioGroupCoches);
+            radioGroupMotos.check(R.id.radioGroupMotos);
     }
+
+    //en el radigroup tiene el Id del bton que se ha pulsado, en "int i" va a indicar
+    ///dentro del radio group el id del radiobutton pulsado porque un R.id. es un entero
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+        Log.i("Radiogroup", "entro");
+
+        if (checkBoxCoches.isChecked()){
+        Log.i("RadiogroupCoches", "entro");
+        if (radioGroup.getId() == R.id.radioGroupCoches) {
+            switch (i) {
+                case R.id.radioButtonCocheDrcha:{
+                    layoutParaCoches = R.layout.car_row_right;
+                    break;}
+                case R.id.radioButtonCocheIzda:{
+                    layoutParaCoches = R.layout.car_row_left;
+                    break;}
+                case R.id.radioButtonCocheDoble:{
+                    layoutParaCoches = R.layout.car_row_double;
+                    break;}
+            }
+        }}
+        if (checkBoxMotos.isChecked())
+            Log.i("RadiogroupMotos", "entro");
+        {        if (radioGroup.getId() == R.id.radioGroupMotos) {
+            switch (i) {
+                case R.id.radioButtonMotoDrcha:{
+                    layoutParaMotos = R.layout.bike_row_right;
+                    break;}
+                case R.id.radioButtonMotoIzda:{
+                    layoutParaMotos = R.layout.bike_row_left;
+                    break;}
+                case R.id.radioButtonMotoDoble:{
+                    layoutParaMotos = R.layout.bike_row_double;
+                    break;}
+            }
+        }}
+        if (checkBoxMotos.isChecked()&&(radioGroupMotos.getId()==R.id.radioGroupMotos) ||
+                checkBoxCoches.isChecked()&& (radioGroupCoches.getId()==R.id.radioGroupCoches)){
+//llamo al adaptador aqui porque solo puede mostrar datos si tiene un layout marcado
+        Log.i("on ChekedChanged", "llamo al adapatador");
+        Adaptador adaptador = new Adaptador(arrayListVehiculos, this, layoutParaMotos,
+                layoutParaCoches);
+        listaViewVehiculos.setAdapter(adaptador);
+    }}
 }
+
+
